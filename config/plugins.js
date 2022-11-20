ffa111b8d4f9323518d61c5e12b00fc6e9074e6e
Razvy Cretu <razvan.cretu97@gmail.com>
2022-11-20 03:47:41 +0200

    switch to @strapi/provider-upload-cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,15 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "local",
+      provider: "cloudinary",
       providerOptions: {
-        sizeLimit: 100000,
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
   },
